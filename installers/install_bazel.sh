f0aeed56bfb47206e21d075fcc97a48e25ccc31c
Wonyong Kim <chokobole33@gmail.com>
2019-04-14 14:13:24 +0900

    Check architecture when installing bazel

--- installers/install_bazel.sh
+++ installers/install_bazel.sh
@@ -3,8 +3,9 @@
 set -e
 
 BAZEL_VERSION=0.20.0
-PLATFORM=`uname -s | tr '[:upper:]' '[:lower:]'`
-FILE="bazel-${BAZEL_VERSION}-installer-${PLATFORM}-x86_64.sh"
+OS=`uname -s | tr '[:upper:]' '[:lower:]'`
+ARCH=`uname -m`
+FILE="bazel-${BAZEL_VERSION}-installer-${OS}-${ARCH}.sh"
 
 wget https://github.com/bazelbuild/bazel/releases/download/${BAZEL_VERSION}/${FILE}
 chmod +x ${FILE}
