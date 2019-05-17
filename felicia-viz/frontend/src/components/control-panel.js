eb946d209121e4931cd9170f61a150698c8a6590
Wonyong Kim <chokobole33@gmail.com>
2019-05-17 16:19:11 +0900

    Add animation to sidebar

--- felicia-viz/frontend/src/components/control-panel.js
+++ felicia-viz/frontend/src/components/control-panel.js
@@ -19,12 +19,7 @@ export default class ControlPanel extends Component {
     store: PropTypes.instanceOf(FeliciaVizStore).isRequired,
   };
 
-  _renderContent() {
-    const { store } = this.props;
-    const { id, type } = store.uiState.activeWindow;
-
-    if (id === null) return null;
-
+  _renderContent = type => {
     switch (type) {
       case UI_TYPES.CameraPanel.name: {
         return <CameraControlPanel />;
@@ -32,16 +27,24 @@ export default class ControlPanel extends Component {
       default:
         return null;
     }
-  }
+  };
 
   render() {
+    const { store } = this.props;
+    const { id, type } = store.uiState.activeWindow;
+
+    let className = '';
+    if (id !== null) {
+      className = 'is-opened';
+    }
+
     return (
       <div id='control-panel'>
         <header>
           <Title>Felicia Viz</Title>
         </header>
 
-        <main>{this._renderContent()}</main>
+        <main className={className}>{this._renderContent(type)}</main>
       </div>
     );
   }
