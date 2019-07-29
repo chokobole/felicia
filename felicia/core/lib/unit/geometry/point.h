6bf368841a0e9cb03b4c05d68a94d480a16140ec
Wonyong Kim <chokobole33@gmail.com>
2019-07-29 16:48:23 +0900

    Fix minor bug: change float -> T

--- felicia/core/lib/unit/geometry/point.h
+++ felicia/core/lib/unit/geometry/point.h
@@ -10,28 +10,28 @@ template <typename T>
 class Point {
  public:
   constexpr Point() = default;
-  constexpr Point(float x, float y) : x_(x), y_(y) {}
+  constexpr Point(T x, T y) : x_(x), y_(y) {}
   constexpr Point(const Point& other) = default;
   Point& operator=(const Point& other) = default;
 
-  void Scale(float s) {
+  void Scale(T s) {
     x_ = s * x_;
     y_ = s * y_;
   }
 
-  void set_xy(float x, float y) {
+  void set_xy(T x, T y) {
     x_ = x;
     y_ = y;
   }
-  void set_x(float x) { x_ = x; }
-  void set_y(float y) { y_ = y; }
+  void set_x(T x) { x_ = x; }
+  void set_y(T y) { y_ = y; }
 
-  constexpr float x() const { return x_; }
-  constexpr float y() const { return y_; }
+  constexpr T x() const { return x_; }
+  constexpr T y() const { return y_; }
 
  private:
-  float x_;
-  float y_;
+  T x_;
+  T y_;
 };
 
 template <typename T>
@@ -48,35 +48,35 @@ template <typename T>
 class Point3 {
  public:
   constexpr Point3() = default;
-  constexpr Point3(float x, float y, float z) : x_(x), y_(y), z_(z) {}
+  constexpr Point3(T x, T y, T z) : x_(x), y_(y), z_(z) {}
   constexpr explicit Point3(const Point<T>& point)
       : x_(point.x()), y_(point.y()), z_(0) {}
   constexpr Point3(const Point3& other) = default;
   Point3& operator=(const Point3& other) = default;
 
-  void Scale(float s) {
+  void Scale(T s) {
     x_ = s * x_;
     y_ = s * y_;
     z_ = s * z_;
   }
 
-  void set_xyz(float x, float y, float z) {
+  void set_xyz(T x, T y, T z) {
     x_ = x;
     y_ = y;
     z_ = z;
   }
-  void set_x(float x) { x_ = x; }
-  void set_y(float y) { y_ = y; }
-  void set_z(float z) { z_ = z; }
+  void set_x(T x) { x_ = x; }
+  void set_y(T y) { y_ = y; }
+  void set_z(T z) { z_ = z; }
 
-  constexpr float x() const { return x_; }
-  constexpr float y() const { return y_; }
-  constexpr float z() const { return z_; }
+  constexpr T x() const { return x_; }
+  constexpr T y() const { return y_; }
+  constexpr T z() const { return z_; }
 
  private:
-  float x_;
-  float y_;
-  float z_;
+  T x_;
+  T y_;
+  T z_;
 };
 
 template <typename T>
@@ -91,6 +91,8 @@ inline bool operator!=(const Point3<T>& lhs, const Point3<T>& rhs) {
 
 typedef Point<float> Pointf;
 typedef Point3<float> Point3f;
+typedef Point<double> Pointd;
+typedef Point3<double> Point3d;
 
 }  // namespace felicia
 
