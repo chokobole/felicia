e52ef0d9f5b2c945357b3240d1d7078f28e9b2ca
Wonyong Kim <chokobole33@gmail.com>
2019-08-14 18:27:25 +0900

    Implement CameraMatrix && DistortionMatrix

---
+++ felicia/slam/camera/camera_matrix.h
@@ -0,0 +1,113 @@
+#ifndef FELICIA_SLAM_CAMERA_CAMERA_MATRIX_H_
+#define FELICIA_SLAM_CAMERA_CAMERA_MATRIX_H_
+
+#include "third_party/chromium/base/logging.h"
+
+#include "felicia/core/lib/math/matrix_util.h"
+#include "felicia/core/lib/unit/geometry/native_matrix_reference.h"
+#include "felicia/core/lib/unit/geometry/point.h"
+
+namespace felicia {
+namespace slam {
+
+template <typename MatrixType>
+class CameraMatrix {
+ public:
+  typedef NativeMatrixRef<MatrixType> MatrixTypeRef;
+  typedef ConstNativeMatrixRef<MatrixType> ConstMatrixTypeRef;
+  typedef typename ConstMatrixTypeRef::ScalarType ScalarType;
+  static_assert(
+      (ConstMatrixTypeRef::Rows == 3 || ConstMatrixTypeRef::Rows == -1) &&
+          (ConstMatrixTypeRef::Cols == 3 || ConstMatrixTypeRef::Cols == -1),
+      "CameraMatrix should be 3x3 matrix, otherwise it should be possibly "
+      "changed to that form.");
+
+  CameraMatrix() = default;
+  explicit CameraMatrix(const MatrixType& matrix) : matrix_(matrix) {
+    ConstMatrixTypeRef matrix_ref(matrix_);
+    CHECK((matrix_ref.empty() ||
+           IsMatrix<3, 3>(matrix_ref.rows(), matrix_ref.cols())));
+  }
+  CameraMatrix(ScalarType fx, ScalarType fy, ScalarType cx, ScalarType cy)
+      : matrix_(ConstMatrixTypeRef::Zero(3, 3)) {
+    set_fx(fx);
+    set_fy(fy);
+    set_cx(cx);
+    set_cy(cx);
+  }
+
+  void operator=(const MatrixType& matrix) {
+    ConstMatrixTypeRef matrix_ref(matrix);
+    CHECK((matrix_ref.empty() ||
+           IsMatrix<3, 3>(matrix_ref.rows(), matrix_ref.cols())));
+    matrix_ = matrix;
+  }
+
+  ScalarType fx() const { return ValueOrZero(0, 0); }
+  void set_fx(ScalarType fx) { MaybeSet(0, 0, fx); }
+
+  ScalarType fy() const { return ValueOrZero(1, 1); }
+  void set_fy(ScalarType fy) { Maybeset(1, 1, fy); }
+
+  ScalarType cx() const { return ValueOrZero(0, 2); }
+  void set_cx(ScalarType cx) { MaybeSet(0, 2, cx); }
+
+  ScalarType cy() const { return ValueOrZero(1, 2); }
+  void set_cy(ScalarType cy) { MaybeSet(1, 2, cy); }
+
+  const MatrixType& matrix() const { return matrix_; }
+  MatrixType& matrix() { return matrix_; }
+
+  void set_matrix(const MatrixType& matrix) { matrix_ = matrix; }
+
+  template <typename T>
+  Point<T> Project(const Point3<T>& point) const {
+    T x = point.x();
+    T y = point.y();
+    T z = point.z();
+    CHECK_NE(z, 0.f);
+    double inv_z = 1.0 / z;
+    T u = fx() * x * inv_z + cx();
+    T v = fy() * y * inv_z + cy();
+    return {u, v};
+  }
+
+  template <typename T>
+  Point3<T> ProjectInverse(const Point<T> point, T depth) const {
+    T u = point.x();
+    T v = point.y();
+    T x = (u - cx()) * depth / fx();
+    T y = (v - cy()) * depth / fy();
+    T z = depth;
+    return {x, y, z};
+  }
+
+ private:
+  ScalarType ValueOrZero(int row, int col) const {
+    ConstMatrixTypeRef matrix_ref(matrix_);
+    if (matrix_ref.empty()) return 0;
+    if (matrix_ref.rows() <= row || matrix_ref.cols() <= col) return 0;
+    return matrix_ref.at(row, col);
+  }
+
+  void MaybeSet(int row, int col, ScalarType value) {
+    MatrixTypeRef matrix_ref(matrix_);
+    if (matrix_ref.empty()) return;
+    if (matrix_ref.rows() <= row || matrix_ref.cols() <= col) return;
+    matrix_ref.at(row, col) = value;
+  }
+
+  MatrixType matrix_;
+};
+
+typedef CameraMatrix<Eigen::Matrix3f> EigenCameraMatrixf;
+typedef CameraMatrix<Eigen::Matrix3d> EigenCameraMatrixd;
+#if defined(HAS_OPENCV)
+typedef CameraMatrix<cv::Mat1f> CvCameraMatrixf;
+typedef CameraMatrix<cv::Mat1d> CvCameraMatrixd;
+#endif
+
+}  // namespace slam
+}  // namespace felicia
+
+#endif  // FELICIA_SLAM_CAMERA_CAMERA_MATRIX_H_
---
+++ felicia/slam/camera/distortion_matrix.h
@@ -0,0 +1,136 @@
+#ifndef FELICIA_SLAM_CAMERA_DISTORTION_MATRIX_H_
+#define FELICIA_SLAM_CAMERA_DISTORTION_MATRIX_H_
+
+#include "felicia/core/lib/unit/geometry/native_matrix_reference.h"
+
+namespace felicia {
+namespace slam {
+
+constexpr bool IsValidDistortionMatrix(int rows, int cols) {
+  return rows == 1 && (cols == 4 || cols == 5 || cols == 6 || cols == 8);
+}
+
+template <typename MatrixType>
+class DistortionMatrix {
+ public:
+  typedef NativeMatrixRef<MatrixType> MatrixTypeRef;
+  typedef ConstNativeMatrixRef<MatrixType> ConstMatrixTypeRef;
+  typedef typename ConstMatrixTypeRef::ScalarType ScalarType;
+  static_assert(
+      IsValidDistortionMatrix(ConstMatrixTypeRef::Rows,
+                              ConstMatrixTypeRef::Cols) ||
+          (ConstMatrixTypeRef::Rows == 1 && ConstMatrixTypeRef::Cols == -1) ||
+          (ConstMatrixTypeRef::Rows == -1 && ConstMatrixTypeRef::Cols == -1),
+      "DistortionMatrix should be 1xn matrix, otherwise it should be "
+      "possibly changed to that form.");
+
+  DistortionMatrix() = default;
+  explicit DistortionMatrix(const MatrixType& matrix) : matrix_(matrix) {
+    ConstMatrixTypeRef matrix_ref(matrix_);
+    CHECK((matrix_ref.empty() ||
+           IsValidDistortionMatrix(matrix_ref.rows(), matrix_ref.cols())));
+  }
+  DistortionMatrix(ScalarType k1, ScalarType k2, ScalarType p1, ScalarType p2)
+      : matrix_(ConstMatrixTypeRef::Zero(1, 4)) {
+    set_k1(k1);
+    set_k2(k2);
+    set_p1(p1);
+    set_p2(p2);
+  }
+  DistortionMatrix(ScalarType k1, ScalarType k2, ScalarType p1, ScalarType p2,
+                   ScalarType k3)
+      : matrix_(ConstMatrixTypeRef::Zero(1, 5)) {
+    set_k1(k1);
+    set_k2(k2);
+    set_p1(p1);
+    set_p2(p2);
+    set_k3(k3);
+  }
+  DistortionMatrix(ScalarType k1, ScalarType k2, ScalarType p1, ScalarType p2,
+                   ScalarType k3, ScalarType k4)
+      : matrix_(ConstMatrixTypeRef::Zero(1, 6)) {
+    set_k1(k1);
+    set_k2(k2);
+    set_p1(p1);
+    set_p2(p2);
+    set_k3(k3);
+    set_k4(k4);
+  }
+  DistortionMatrix(ScalarType k1, ScalarType k2, ScalarType p1, ScalarType p2,
+                   ScalarType k3, ScalarType k4, ScalarType k5, ScalarType k6)
+      : matrix_(ConstMatrixTypeRef::Zero(1, 8)) {
+    set_k1(k1);
+    set_k2(k2);
+    set_p1(p1);
+    set_p2(p2);
+    set_k3(k3);
+    set_k4(k4);
+    set_k5(k5);
+    set_k6(k6);
+  }
+
+  void operator=(const MatrixType& matrix) {
+    ConstMatrixTypeRef matrix_ref(matrix);
+    CHECK((matrix_ref.empty() ||
+           IsValidDistortionMatrix(matrix_ref.rows(), matrix_ref.cols())));
+    matrix_ = matrix;
+  }
+
+  ScalarType k1() const { return ValueOrZero(0); }
+  void set_k1(ScalarType k1) { MaybeSet(0, k1); }
+
+  ScalarType k2() const { return ValueOrZero(1); }
+  void set_k2(ScalarType k2) { MaybeSet(1, k2); }
+
+  ScalarType p1() const { return ValueOrZero(2); }
+  void set_p1(ScalarType p1) { MaybeSet(2, k2); }
+
+  ScalarType p2() const { return ValueOrZero(3); }
+  void set_p2(ScalarType p2) { MaybeSet(3, p2); }
+
+  ScalarType k3() const { return ValueOrZero(4); }
+  void set_k3(ScalarType k3) { MaybeSet(4, k3); }
+
+  ScalarType k4() const { return ValueOrZero(5); }
+  void set_k4(ScalarType k4) { MaybeSet(5, k4); }
+
+  ScalarType k5() const { return ValueOrZero(6); }
+  void set_k5(ScalarType k5) { MaybeSet(6, k5); }
+
+  ScalarType k6() const { return ValueOrZero(7); }
+  void set_k6(ScalarType k6) { MaybeSet(7, k6); }
+
+  const MatrixType& matrix() const { return matrix_; }
+  MatrixType& matrix() { return matrix_; }
+
+  void set_matrix(const MatrixType& matrix) { matrix_ = matrix; }
+
+ private:
+  ScalarType ValueOrZero(int index) const {
+    ConstMatrixTypeRef matrix_ref(matrix_);
+    if (matrix_ref.empty()) return 0;
+    if (matrix_ref.cols() <= index) return 0;
+    return matrix_ref.at(0, index);
+  }
+
+  void MaybeSet(int index, ScalarType value) {
+    MatrixTypeRef matrix_ref(matrix_);
+    if (matrix_ref.empty()) return;
+    if (matrix_ref.cols() <= index) return;
+    matrix_ref.at(0, index) = value;
+  }
+
+  MatrixType matrix_;
+};
+
+typedef DistortionMatrix<Eigen::VectorXf> EigenDistortionMatrixf;
+typedef DistortionMatrix<Eigen::VectorXd> EigenDistortionMatrixd;
+#if defined(HAS_OPENCV)
+typedef DistortionMatrix<cv::Mat1f> CvDistortionMatrixf;
+typedef DistortionMatrix<cv::Mat1d> CvDistortionMatrixd;
+#endif
+
+}  // namespace slam
+}  // namespace felicia
+
+#endif  // FELICIA_SLAM_CAMERA_DISTORTION_MATRIX_H_
