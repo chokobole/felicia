4146023ec62238c3e48fb83d8e30bd54fc346023
Wonyong Kim <chokobole33@gmail.com>
2019-09-03 15:35:26 +0900

    Update pool benchmark

--- felicia/core/lib/containers/pool_benchmark.cc
+++ felicia/core/lib/containers/pool_benchmark.cc
@@ -24,7 +24,7 @@ class QueueWithSize {
     queue_.push(std::move(value));
   }
 
-  ALWAYS_INLINE void pop() { queue_, pop(); }
+  ALWAYS_INLINE void pop() { queue_.pop(); }
 
  private:
   uint8_t size_;
@@ -39,20 +39,49 @@ template <typename QueueType>
 static void BM_Push(benchmark::State& state) {
   int size = state.range(0);
   for (auto _ : state) {
-    QueueType pool(255);
+    QueueType pool(10);
     for (int i = 0; i < size; i++) {
       pool.push(i);
     }
   }
 }
 
-BENCHMARK_TEMPLATE(BM_Push, Uint8Pool)->Arg(1000);
-BENCHMARK_TEMPLATE(BM_Push, QueueWithSize<int>)->Arg(1000);
+template <typename QueueType>
+static void BM_PushAndPop(benchmark::State& state) {
+  int size = state.range(0);
+  for (auto _ : state) {
+    QueueType pool(10);
+    for (int i = 0; i < size; i++) {
+      if (i % 2 == 0) {
+        pool.push(i);
+      } else {
+        pool.pop();
+      }
+    }
+  }
+}
 
-// --------------------------------------------------------------------------
-// Benchmark                                   Time           CPU Iterations
-// --------------------------------------------------------------------------
-// BM_Push<Uint8Pool>/100000               0 ns          0 ns 1000000000
-// BM_Push<QueueWithSize<int>>/100000     264622 ns     264012 ns       2650
+BENCHMARK_TEMPLATE(BM_Push, Uint8Pool)->Arg(10000);
+BENCHMARK_TEMPLATE(BM_Push, QueueWithSize<int>)->Arg(10000);
+BENCHMARK_TEMPLATE(BM_PushAndPop, Uint8Pool)->Arg(10000);
+BENCHMARK_TEMPLATE(BM_PushAndPop, QueueWithSize<int>)->Arg(10000);
+
+// clang-format off
+// 2019-09-03 15:29:19
+// Running bazel-bin/felicia/core/lib/pool_benchmark
+// Run on (8 X 4900 MHz CPU s)
+// CPU Caches:
+//   L1 Data 32K (x8)
+//   L1 Instruction 32K (x8)
+//   L2 Unified 256K (x8)
+//   L3 Unified 12288K (x1)
+// -------------------------------------------------------------------------------
+// Benchmark                                        Time           CPU Iterations
+// -------------------------------------------------------------------------------
+// BM_Push<Uint8Pool>/10000                     11437 ns      11437 ns      61290
+// BM_Push<QueueWithSize<int>>/10000            30471 ns      30469 ns      22466
+// BM_PushAndPop<Uint8Pool>/10000                7561 ns       7556 ns      91394
+// BM_PushAndPop<QueueWithSize<int>>/10000      14565 ns      14565 ns      47995
+// clang-format on
 
 }  // namespace felicia
