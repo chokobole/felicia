719b21c6fd0552d9f30e9d9840bb0d273daecb17
Wonyong Kim <chokobole33@gmail.com>
2019-09-16 15:38:27 +0900

    Change EventType && and extend activatable panel

--- felicia-viz/modules/ui/src/components/common/activatable.js
+++ felicia-viz/modules/ui/src/components/common/activatable.js
@@ -7,11 +7,18 @@ export default class Activatable extends PureComponent {
   static propTypes = {
     id: PropTypes.number.isRequired,
     type: PropTypes.string.isRequired,
+    width: PropTypes.string,
+    height: PropTypes.string,
     children: PropTypes.node.isRequired,
     uiState: PropTypes.instanceOf(UIState).isRequired,
   };
 
-  _onMouseDown = () => {
+  static defaultProps = {
+    width: '100%',
+    height: '100%',
+  };
+
+  _onPointerDown = () => {
     const { id, type, uiState } = this.props;
 
     console.log(`${id} is activated.`);
@@ -19,10 +26,10 @@ export default class Activatable extends PureComponent {
   };
 
   render() {
-    const { children } = this.props;
+    const { width, height, children } = this.props;
 
     return (
-      <div onMouseDown={this._onMouseDown} role='none'>
+      <div style={{ width, height }} onPointerDown={this._onPointerDown} role='none'>
         {children}
       </div>
     );
