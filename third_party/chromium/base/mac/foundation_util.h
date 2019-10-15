ed10a138d799d48cae095cf3f10b2a6a4ebbb86e
wonyongkim <chokobole33@gmail.com>
2019-10-15 12:34:40 +0900

    Fix broken build for xcode 11.1

--- third_party/chromium/base/mac/foundation_util.h
+++ third_party/chromium/base/mac/foundation_util.h
@@ -52,7 +52,7 @@ typedef CR_FORWARD_ENUM(unsigned int, NSSearchPathDirectory);
 typedef unsigned int NSSearchPathDomainMask;
 #endif
 
-#if defined(OS_IOS)
+#if defined(OS_IOS) || defined(MAC_OS_X_VERSION_10_15)
 typedef struct CF_BRIDGED_TYPE(id) __SecCertificate* SecCertificateRef;
 typedef struct CF_BRIDGED_TYPE(id) __SecKey* SecKeyRef;
 typedef struct CF_BRIDGED_TYPE(id) __SecPolicy* SecPolicyRef;
